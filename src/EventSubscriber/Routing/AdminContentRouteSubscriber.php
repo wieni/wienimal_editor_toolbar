4f0ed70523aac453277cb66a7c00796fdf625d11
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-10-23 11:14:20 +0200

    Move altering of route from routing.yml to an event subscriber

---
+++ src/EventSubscriber/Routing/AdminContentRouteSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\wienimal_editor_toolbar\EventSubscriber\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
+use Symfony\Component\Routing\RouteCollection;
+
+class AdminContentRouteSubscriber extends RouteSubscriberBase
+{
+    public static function getSubscribedEvents()
+    {
+        $events[RoutingEvents::ALTER] = ['onAlterRoutes'];
+
+        return $events;
+    }
+
+    protected function alterRoutes(RouteCollection $collection)
+    {
+        if ($route = $collection->get('system.admin_content')) {
+            $permission = implode('+', array_filter([
+                $route->getRequirement('_permission'),
+                'access editor toolbar',
+            ]));
+
+            $route->setRequirement('_permission', $permission);
+        }
+    }
+}
